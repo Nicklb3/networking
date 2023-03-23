3541f0aaa17173ddb68c0af803382b1eaf6bdfd4
Nicholas Bonner <nicholasbonner@Nicholass-Mac-mini.local>
2023-03-23 13:07:23 -0700

    made a userSchema

--- models/User.js
+++ models/User.js
@@ -1,4 +1,50 @@
-const User = require('./User');
-const Thought = require('./Thought');
+const { Schema, model } = require('mongoose');
 
-module.exports = { User, Thought };
+
+
+// schema to create user model
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+
+        },
+        email: {
+            type: String, 
+            required: true,
+            unique: true,
+            match: [/.+@.+\..+/, "Please enter a vaild email address."],
+        },
+        thoughts: [ 
+            {
+            type: Schema.Types.ObjectId,
+            ref: 'thought',
+        },
+    ],
+        friends: [
+            {
+            type: Schema.Types.ObjectId, 
+            ref: 'user',
+        }
+    ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+// virtual property (not stored in MongoDB) of a user's friend count
+userSchema.virtual('friendcount').get(function () {
+    return this.friends.length;
+});
+
+// create User model with schema
+const User = model('user', userSchema);
+
+module.exports = User;
