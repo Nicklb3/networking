76a7778323c51ecc2628f0c1e438db2a848fd12d
Nicholas Bonner <nicholasbonner@Nicholass-Mac-mini.local>
2023-03-23 12:51:47 -0700

    commented code in user routes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -10,10 +10,13 @@ const {
     deleteFriend,
 } = require('../../controllers/userController');
 
+// create a user
 router.route('/').get(getUsers).post(createUser);
 
+// update a user
 router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
 
+// add/delete a friend
 router.route('/:userId/friends/:friendId').put(addFriend).delete(deleteFriend);
 
 module.exports = router;
