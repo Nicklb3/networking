39eb3e5b19687d40032cda9d4cb4f4b107053223
Nicholas Bonner <nicholasbonner@Nicholass-Mac-mini.local>
2023-03-23 12:55:47 -0700

    added comments to thoughtsRoute

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -10,12 +10,16 @@ const {
     deleteReaction,
 } = require('../../controllers/thoughtController');
 
+// get all thoughts or create a thought
 router.route('/').get(getThoughts).post(createThought);
 
+// get or update single thought
 router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
 
+// add reaction
 router.route('/:thoughtId/reactions').put(addReaction);
 
+// delete reaction
 router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
 
 module.exports = router;
